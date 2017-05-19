c307ea20961770c5b494133d44851cae17d504d5
rishabh jain <rishabh.jain.rishu@gmail.com>
2017-05-19 12:54:32 +0530

    Delete header.h

--- header.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>                                           //HEADER FILE
-
-void prof();                                                //FUNCTION DECLARATION
-void lucky();                                               //FUNCTION DECLARATION
-void display1(int ,int,int);                                //FUNCTION DECLARATION
-int check(int, FILE **);                                    //FUNCTION DECLARATION
-long unsigned int add(long unsigned int);                   //FUNCTION DECLARATION
-long unsigned int diff(long unsigned int);                  //FUNCTION DECLARATION
-void display2( long unsigned int,long unsigned int);        //FUNCTION DECLARATION
