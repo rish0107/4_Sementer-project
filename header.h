cf66b8983405b0b13a6baeebcd5b9f367026d337
rishabh jain <rishabh.jain.rishu@gmail.com>
2017-06-29 15:14:06 +0530

    Add files via upload

---
+++ header.h
@@ -0,0 +1,9 @@
+#include <iostream>                             //HEADER FILE
+#include<fstream>                               //HEADER FILE
+#include<string.h>                              //HEADER FILE
+
+using namespace std;
+
+void minsp(int [1002][1002],int);               //FUNCTION DECLARATION
+void user(int [1002][1002],ifstream &);         //FUNCTION DECLARATION
+int check(char []);                             //FUNCTION DECLARATION
